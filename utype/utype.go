e3d3a314b97c0e12ff16a470060b530116bb4ca5
zhaoy <ace.zhaoy@gmail.com>
2024-05-14 19:15:32 +0800

    增加 Dereference

--- utype/utype.go
+++ utype/utype.go
@@ -4,7 +4,15 @@ func Pointer[T any](p T) *T {
 	return &p
 }
 
-func Dereference[T any](p *T, defaultValue T) T {
+func Dereference[T any](p *T) T {
+	if p != nil {
+		return *p
+	}
+	var t T
+	return t
+}
+
+func DereferenceOrDefault[T any](p *T, defaultValue T) T {
 	if p != nil {
 		return *p
 	}
