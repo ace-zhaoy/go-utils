fee250d67c3e36ae212db73da73e69a9221330dc
zhaoy <ace.zhaoy@gmail.com>
2024-05-14 19:01:45 +0800

    增加 Dereference

--- utype/utype.go
+++ utype/utype.go
@@ -3,3 +3,10 @@ package utype
 func Pointer[T any](p T) *T {
 	return &p
 }
+
+func Dereference[T any](p *T, defaultValue T) T {
+	if p != nil {
+		return *p
+	}
+	return defaultValue
+}
--- utype/utype_test.go
+++ utype/utype_test.go
@@ -1,6 +1,7 @@
 package utype
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +21,50 @@ func TestPointer(t *testing.T) {
 
 	if *ptr != testInstance {
 		t.Errorf("Pointer value does not match the original struct")
+	}
+}
+
+func TestDereference(t *testing.T) {
+	type args[T any] struct {
+		p            *T
+		defaultValue T
+	}
+	type testCase[T any] struct {
+		name string
+		args args[T]
+		want T
+	}
+	tests := []testCase[int]{
+		{
+			name: "test1",
+			args: args[int]{
+				p:            Pointer(5),
+				defaultValue: 10,
+			},
+			want: 5,
+		},
+		{
+			name: "test2",
+			args: args[int]{
+				p:            nil,
+				defaultValue: 10,
+			},
+			want: 10,
+		},
+		{
+			name: "test3",
+			args: args[int]{
+				p:            Pointer(0),
+				defaultValue: 10,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Dereference(tt.args.p, tt.args.defaultValue); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Dereference() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
